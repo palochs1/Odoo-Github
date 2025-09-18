c157f3f7ade6a628c7d54a9e2f92e3112f29c9fc
Paloch Sutayasaranakom <Paloch@PhutipongERP-Mac.local>
2025-09-18 14:38:44 +0700

    addons my_demo

---
+++ addons/my_demo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "My Demo",
+    "version": "15.0.1.0.0",
+    "author": "You",
+    "category": "Tools",
+    "summary": "Demo module from Git",
+    "depends": ["base"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
